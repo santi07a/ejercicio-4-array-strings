b318ec34100b5e69bc1c75b9653128e22c3f210b
Santi Arnau <santi07a@gmail.com>
2021-02-21 15:22:55 +0100

    agregado función anónima y cambio de nombre al parámetro

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,15 @@
-console.log("hello kitty")
+let palabras = ['café', 'croissant', 'huevo', 'jugo', 'leche', 'avena'];
+let palabrasParesModificadas = modificaPares(palabras, elemento => elemento.split("").join("-"));
+console.log(palabrasParesModificadas);
+
+function modificaPares(cosas, funcionAnonima) {
+  let palabrasParesModificadas = [];
+  for (const i in cosas) {
+    if (i % 2 !== 0) {
+      palabrasParesModificadas.push(funcionAnonima(cosas[i]));
+    } else {
+      palabrasParesModificadas.push(cosas[i]);
+    };
+  }
+  return palabrasParesModificadas;
+};
